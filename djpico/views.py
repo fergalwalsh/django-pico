6f58c15e83c1d6a7453db17f7b86a55a8d65fc4d
Fergal Walsh <fergal.walsh@metglobal.com>
2014-07-24 16:20:53 +0300

    Improved exception handling

--- djpico/views.py
+++ djpico/views.py
@@ -1,4 +1,6 @@
 import pico.server
+from pico import PicoError
+
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
@@ -12,8 +14,16 @@ def index(request):
     params.update(request.GET.dict())
     params.update(request.POST.dict())
     path = '/' + request.path.split('/pico/')[-1]
-    pico_response = pico.server.handle_api_v2(path, params, request)
-    response = HttpResponse(pico_response.output)
+    try:
+        pico_response = pico.server.handle_api_v2(path, params, request)
+    except PicoError, e:
+        pico_response = e.response
+    except Exception, e:
+        pico_response = pico.server.generate_exception_report(e, path, params)
+    code, reason = pico_response.status.split(' ', 1)
+    response = HttpResponse(pico_response.output,
+                            status=int(code),
+                            reason=reason)
     for key, header in pico_response.headers:
         response[key] = header
     return response
